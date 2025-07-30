1506a93e629eb80a932fb41b006c9ea792a3cc0e
xblam <benlam05032004@gmail.com>
2025-07-31 00:17:44 +0700

    making preprocessing function

--- notebooks/placement.py
+++ notebooks/placement.py
@@ -1,4 +1,3 @@
-# chose the columns that we want to keep
 # can choose which columns to keep or drop
 train = train[[
     'PassengerId', 
@@ -12,15 +11,4 @@ train = train.rename(columns={
     'PassengerId': 'Id',
     'Pclass': 'Class'})
 
-
-# first we change sex into binary
-train['Sex'] = train['Sex'].map({'male':1, 'female':0})
-
-# only grab features that I want
-train = train[[ 'Survived', 'Class',  'Sex', 'Age', 'SibSp', 'Parch',
-    'Fare', 'Embarked']]
-train['Embarked'] = train['Embarked'].astype('category')
-
-train.head()
-
-df_numeric = pd.get_dummies(train, columns=['Class', 'Embarked', ], drop_first=True).copy()
+train = train.drop(columns=['AgeGroup', 'Title'])
